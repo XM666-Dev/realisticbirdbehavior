b659940db3bfe2a2f87b9202b02adfec0cc2e7b6
XM666-Dev <x94732568960@163.com>
2025-10-22 09:45:26 +0800

    Fix shoulder rendering

--- src/main/java/com/xm666/realisticbirdbehavior/mixin/ModelMixin.java
+++ src/main/java/com/xm666/realisticbirdbehavior/mixin/ModelMixin.java
@@ -3,42 +3,24 @@ package com.xm666.realisticbirdbehavior.mixin;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.xm666.realisticbirdbehavior.Config;
 import net.minecraft.client.model.HierarchicalModel;
 import net.minecraft.client.model.ParrotModel;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.layers.ParrotOnShoulderLayer;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.Parrot;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public class ModelMixin {
-    //@Mixin(ParrotModel.class)
-    //private static class ParrotModelMixin {
-    //    @Shadow
-    //    @Final
-    //    private ModelPart body;
-
-    //    @Inject(method = "prepareMobModel(Lnet/minecraft/world/entity/animal/Parrot;FFF)V", at = @At("TAIL"))
-    //    private void prepare(Parrot entity, float limbSwing, float limbSwingAmount, float partialTick, CallbackInfo ci) {
-    //        var self = (ParrotModel) (Object) this;
-    //        self.young = entity.getAge() < 0;
-    //        RealisticBirdBehavior.LOGGER.info("Parrot Model preparing and young is {}", self.young);
-    //        if (self.young) {
-    //            this.body.xScale = 0.5F;
-    //            this.body.yScale = 0.5F;
-    //            this.body.zScale = 0.5F;
-    //        } else {
-    //            this.body.xScale = 1.0F;
-    //            this.body.yScale = 1.0F;
-    //            this.body.zScale = 1.0F;
-    //        }
-    //    }
-    //}
-
-    //@Mixin(EntityModel.class)
-    //private static class EntityModelMixin{
-    //    @Inject(method = "render")
-    //}
+    private static final float youngScaleFactor = 0.5F;
+    private static final float bodyYOffset = 24.0F;
+    private static final float bodyXOffset = 0.4F;
 
     @Mixin(Parrot.class)
     private static class ParrotMixin {
@@ -55,8 +37,6 @@ public class ModelMixin {
         private void renderToBuffer(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, int color, CallbackInfo ci) {
             var self = (HierarchicalModel<?>) (Object) this;
             if (self instanceof ParrotModel && self.young) {
-                var youngScaleFactor = 0.5F;
-                var bodyYOffset = 24.0F;
                 poseStack.pushPose();
                 poseStack.scale(youngScaleFactor, youngScaleFactor, youngScaleFactor);
                 poseStack.translate(0.0F, bodyYOffset / 16.0F, 0.0F);
@@ -67,31 +47,15 @@ public class ModelMixin {
         }
     }
 
-    @Mixin(ParrotModel.class)
-    private static class ParrotModelMixin {
-        @Inject(method = "renderOnShoulder", at = @At("HEAD"), cancellable = true)
-        private void renderOnShoulder(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, float limbSwing, float limbSwingAmount, float netHeadYaw, float headPitch, int tickCount, CallbackInfo ci) {
-            var self = (ParrotModel) (Object) this;
-            if (self.young) {
-                var youngScaleFactor = 0.5F;
-                var bodyYOffset = 24.0F;
-                poseStack.pushPose();
+    @Mixin(ParrotOnShoulderLayer.class)
+    private static class ParrotOnShoulderLayerMixin {
+        @Inject(method = "lambda$render$1", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;pushPose()V", shift = At.Shift.AFTER))
+        private void render(PoseStack poseStack, boolean leftShoulder, Player livingEntity, CompoundTag compoundtag, MultiBufferSource buffer, int packedLight, float limbSwing, float limbSwingAmount, float netHeadYaw, float headPitch, EntityType<?> p_262538_, CallbackInfo ci) {
+            if (compoundtag.getInt("Age") < 0) {
+                var bodyXOffset = ModelMixin.bodyXOffset - Config.SHOULDER_BODY_X_OFFSET.get();
                 poseStack.scale(youngScaleFactor, youngScaleFactor, youngScaleFactor);
-                poseStack.translate(0.0F, bodyYOffset / 16.0F, 0.0F);
-                self.prepare(ParrotModel.State.ON_SHOULDER);
-                self.setupAnim(ParrotModel.State.ON_SHOULDER, tickCount, limbSwing, limbSwingAmount, 0.0F, netHeadYaw, headPitch);
-                self.root().render(poseStack, buffer, packedLight, packedOverlay);
-                poseStack.popPose();
-                ci.cancel();
+                poseStack.translate(leftShoulder ? bodyXOffset : -bodyXOffset, 0.0F, 0.0F);
             }
         }
-
-        //@Inject(method = "renderOnShoulder", at = @At("TAIL"))
-        //private void renderOnShoulderTail(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, float limbSwing, float limbSwingAmount, float netHeadYaw, float headPitch, int tickCount, CallbackInfo ci) {
-        //    var self = (ParrotModel) (Object) this;
-        //    if (self.young) {
-        //        poseStack.popPose();
-        //    }
-        //}
     }
 }
